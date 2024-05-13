eab40247ba33e44b3a09c67dfb8057fced41f1a1
emlai1263 <emlai1263@gmail.com>
2024-05-13 14:30:00 -0700

    initial commit

---
+++ Assets/Scripts/LineController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode()]
+public class LineController : MonoBehaviour {
+
+    public Transform[] points;
+    public LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start () {
+        lineRenderer.positionCount = points.Length;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    for (int i = 0; i < points.Length; ++i)
+	    {
+            lineRenderer.SetPosition(i, points[i].position);
+	    }
+    }
+}
